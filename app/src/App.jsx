7fa8dfc6c34bc87e2d1182168b60027ed307d4be
Joshua Oladeji <dejijolaoluwa@gmail.com>
2022-04-16 15:12:23 +0100

    designed dashboard interface

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import Nav from './components/Nav.jsx';
 import Stake from './components/Stake.jsx';
@@ -6,13 +6,35 @@ import Dashboard from './components/Dashboard.jsx';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
+  const [liquidity, setLiquidity] = useState(185.267);
+  const [stake, setStake] = useState(25);
+  const [share, setShare] = useState(25);
+  const [reward, setReward] = useState(0);
+  const calculateShare = (contribution, total) => {
+    return (contribution / total) * 100;
+  }
+
+  useEffect(() => {
+    setShare(() => calculateShare(stake, liquidity));
+  }, [stake, liquidity]);
+
   return (
     <div className="App">
       <Nav />
       <Routes>
         <Route path="/" element={<Stake />} />
         <Route path="/stake" element={<Stake />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            <Dashboard
+              liquidity={liquidity}
+              stake={stake}
+              share={share}
+              reward={reward}
+            />
+          }
+        />
       </Routes>
       <p className="add-to-wallet">Add ATRAC to Wallet</p>
     </div>
--- app/src/components/Dashboard.jsx
+++ app/src/components/Dashboard.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Dashboard.css';
+import Timer from './Timer.jsx';
 
-const Dashboard = (props) => {
+const Dashboard = ({ liquidity, stake, share, reward }) => {
+  const withdrawals = [
+    { 'time': 180, 'size': 1.2, 'info': 'nothing for now' },
+    { 'time': 150, 'size': 1.2, 'info': 'nothing for now' },
+    { 'time': 120, 'size': 1.2, 'info': 'nothing for now' }
+  ];
+  const [action, setAction] = useState('Withdraw');
+
   return (
     <div className="dashboard">
-      <h1>Dashboard</h1>
+      <h3>Dashboard</h3>
+      <div className="content">
+        <div className="left-side">
+          {withdrawals.map((epoch, index) => (
+            <div className="timer-padding">
+              <Timer
+                seconds={epoch['time']}
+                size={epoch['size']}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="right-side">
+          <div className="info-container">
+            <h3>Pool Liquidity:</h3>
+            <span>
+              {liquidity}
+            </span>
+          </div>
+          <div className="info-container">
+            <h3>Your Stake:</h3>
+            <span>
+              {stake}
+            </span>
+          </div>
+          <div className="info-container">
+            <h3>Pool Share:</h3>
+            <span>
+              {share}%
+            </span>
+          </div>
+          <div className="info-container">
+            <h3>Potential Reward:</h3>
+            <span>
+              {reward}
+            </span>
+          </div>
+          <div className="dashboard-buttons">
+            <button>
+              {action}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
